ae771f03704a1d8ad34cc73f5ca8da57e20ed5a4
fefifofum <jorsellis@gmail.com>
2015-10-27 19:28:15 +0100

    Add camera wrapper

--- device.mk
+++ device.mk
@@ -51,6 +51,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qualcomm.bt.hci_transport=smd
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8960
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
