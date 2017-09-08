eb46fed200bba984ce852a9339d981186ab6aa46
Adriano Martins <adrianomartins@gmail.com>
2017-09-07 22:23:00 -0300

    Update build script

--- make_v500.sh
+++ make_v500.sh
@@ -1,11 +1,19 @@
-. build/envsetup.sh
+# Sync
+repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
 
-lunch aosp_v500-userdebug
-
+# System Config
 #export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx3g"
 ./prebuilts/sdk/tools/jack-admin kill-server
 ./prebuilts/sdk/tools/jack-admin start-server
 
+# Set up environment
+. build/envsetup.sh
+
+# Target
+lunch aosp_v500-userdebug
+
+# Build
+#mka bacon -j8
 #make clean
 #make bootimage
 make -j8 otapackage
