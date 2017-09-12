c92f3221be03127097c39d83bd7121c30cec7148
svlog <gnuequal@gmail.com>
2017-09-12 16:37:13 +0200

    added HIDL
    Signed-off-by: svlog <gnuequal@gmail.com>

--- device.mk
+++ device.mk
@@ -241,6 +241,23 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \    
     ro.product.first_api_level=24
     
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.drm@1.0-impl \
+    android.hardware.gnss@1.0-impl \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.light@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.nfc@1.0-impl \
+    android.hardware.renderscript@1.0-impl \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.wifi@1.0-service    
+    
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-filter=speed \
