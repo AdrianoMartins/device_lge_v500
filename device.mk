c5d1814613dcc1c6239ef442d7c365ec13ab48bd
Adriano Martins <adrianomartins@gmail.com>
2017-10-07 23:55:52 -0300

    Add qcom bt hal

--- device.mk
+++ device.mk
@@ -260,7 +260,11 @@ PRODUCT_PACKAGES += \
     
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service    
+    android.hardware.usb@1.0-service
+    
+# BT HAL
+PRODUCT_PACKAGES += \
+    libbt-vendor        
     
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
